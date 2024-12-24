88e2770b70c2a261d73000c32e656d5d8de4ce50
cubysot <cubysoft@cubysoft.uk>
2024-12-23 21:19:30 -0500

    Update version and enhance Marquee and ServicesHome components with new styles and content

--- src/components/pages/SliderHome.js
+++ src/components/pages/SliderHome.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './Marquee.css'; // Asegúrate de crear este archivo para estilos
-import { FaFacebook, FaDiscord, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa'; // Importa los iconos que necesites
+
+import { VscAzure } from "react-icons/vsc";
+import { FaAws } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { SiFortinet } from "react-icons/si";
+import { SiMcafee } from "react-icons/si";
+import { SiKaspersky } from "react-icons/si";
+import { SiOracle } from "react-icons/si";
+import { FaCloudflare } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaGitlab } from "react-icons/fa";
+import { SiAuth0 } from "react-icons/si";
+
+
 
 const companies = [
-    { id: 1, name: 'Facebook', icon: <FaFacebook /> },
-    { id: 2, name: 'Discord', icon: <FaDiscord /> },
-    { id: 3, name: 'GitHub', icon: <FaGithub /> },
-    { id: 4, name: 'Twitter', icon: <FaTwitter /> },
-    { id: 5, name: 'Instagram', icon: <FaInstagram /> },
+    { id: 1, name: 'Azure', icon: <VscAzure />, color: '#0078D4' },
+    { id: 2, name: 'Aws', icon: <FaAws />, color: '#FF9900' },
+    { id: 3, name: 'Google', icon: <FcGoogle />, color: '' },
+    { id: 4, name: 'Fortinet', icon: <SiFortinet />, color: '#EE3124' },
+    { id: 5, name: 'Mcafee', icon: <SiMcafee />, color: '#C8102E' },
+    { id: 6, name: 'Kaspersky', icon: <SiKaspersky />, color: '#006C67' },
+    { id: 7, name: 'Oracle', icon: <SiOracle />, color: '#F80000' },
+    { id: 8, name: 'Cloudflare', icon: <FaCloudflare />, color: '#F38020' },
+    { id: 9, name: 'Github', icon: <FaGithub />, color: '#000000' },
+    { id: 10, name: 'Gitlab', icon: <FaGitlab />, color: '#FC6D26' },
+    { id: 11, name: 'Auth0', icon: <SiAuth0 />, color: '#000' },
 ];
+
 
 const Marquee = () => {
     return (
@@ -16,12 +36,12 @@ const Marquee = () => {
             <div className="marquee-content">
                 {companies.map(company => (
                     <div className="marquee-item" key={company.id} title={company.name}>
-                        {company.icon}
+                        <span style={{ color: company.color }}>{company.icon}</span>
                     </div>
                 ))}
                 {companies.map(company => (
                     <div className="marquee-item" key={company.id + 100}>
-                        {company.icon}
+                        <span style={{ color: company.color }}>{company.icon}</span>
                     </div>
                 ))}
             </div>
